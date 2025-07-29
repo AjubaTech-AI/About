b006bc9e2a149d2ebbdd3d9528840a12d99dd69b
AjubaTech <info@ajubatech.com>
2025-07-29 18:51:26 +1000

    Update ThankYou.tsx

--- src/pages/ThankYou.tsx
+++ src/pages/ThankYou.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-const ThankYou: React.FC = () => (
-  <div className="max-w-lg mx-auto p-8 text-center">
+const ThankYou = () => (
+  <div className="text-center p-10">
     <h1 className="text-3xl font-bold mb-4">Thank You!</h1>
-    <p>We received your message and will get back to you shortly.</p>
+    <p>We have received your message and will get back to you soon.</p>
   </div>
 );
-
 export default ThankYou;
