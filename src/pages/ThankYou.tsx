407c3b6174e04fea3f052ec67958d282902cf644
AjubaTech <info@ajubatech.com>
2025-07-29 18:41:00 +1000

    Create ThankYou.tsx
    Thankyou

---
+++ src/pages/ThankYou.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ThankYou: React.FC = () => (
+  <div className="max-w-lg mx-auto p-8 text-center">
+    <h1 className="text-3xl font-bold mb-4">Thank You!</h1>
+    <p>We received your message and will get back to you shortly.</p>
+  </div>
+);
+
+export default ThankYou;
