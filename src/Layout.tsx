39235cdf14854ac4b80ad9fab0ee649d70b75c58
AjubaTech <info@ajubatech.com>
2025-07-29 18:14:10 +1000

    Update Layout.tsx
    footer

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,24 +1,16 @@
 // src/Layout.tsx
 import React from "react";
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <>
       <Navigation />
       <main>{children}</main>
+      <Footer />
     </>
   );
 };
 
 export default Layout;
-
-import Footer from './components/Footer';
-
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <>
-    <Navigation />
-    <main>{children}</main>
-    <Footer />
-  </>
-);
